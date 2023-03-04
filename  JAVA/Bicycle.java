820e2d57062cd5335b53bb1f4728f4c8e4adb407
onilemon009 <onilemon009@gmail.com>
2023-03-04 16:16:07 +0900

    コード修正

---  JAVA/Bicycle.java
+++  JAVA/Bicycle.java
@@ -1,17 +1,6 @@
-class Bicycle {
-  private String name;
-  private String color;
-  private int distance = 0;
-
+class Bicycle extends Vehicle {
   Bicycle(String name, String color) {
-    this.name = name;
-    this.color = color;
-  }
-
-  public void printData() {
-    System.out.println("名前：" + this.name);
-    System.out.println("色：" + this.color);
-    System.out.println("走行距離：" + this.distance + "km");
+    super(name, color);
   }
 
   public void run(int distance) {
