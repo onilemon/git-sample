8b4cdfe0b499d64b06927686ffe1af73ba1f618d
onilemon009 <onilemon009@gmail.com>
2023-03-09 18:52:52 +0900

    first comment

--- progate/ JAVA/Car.java
+++ progate/ JAVA/Car.java
@@ -1,18 +1,16 @@
-class Car {
-  private String name;
-  private String color;
-  private int distance = 0;
-  private int fuel = 100;
+class Car extends Vehicle {
+  private int fuel = 50;
 
-  Car (String name, String color) {
-    this.name = name;
-    this.color = color;
+  Car(String name, String color) {
+    super(name, color);
+  }
+
+  public int getFuel() {
+    return this.fuel;
   }
 
   public void printData() {
-    System.out.println("名前：" + this.name);
-    System.out.println("色：" + this.color);
-    System.out.println("走行距離：" + this.distance + "km");
+    super.printData();
     System.out.println("ガソリン量：" + this.fuel + "L");
   }
 
@@ -24,7 +22,6 @@ class Car {
     } else {
       System.out.println("ガソリンが足りません");
     }
-
     System.out.println("走行距離：" + this.distance + "km");
     System.out.println("ガソリン量：" + this.fuel + "L");
   }
