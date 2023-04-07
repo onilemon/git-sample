261784a09196b08fb1b4ab41dec46d96835cbe9d
onilemon009 <onilemon009@gmail.com>
2023-04-07 16:42:08 +0900

    コード修正

--- progate/php/index1.php
+++ progate/php/index1.php
@@ -1,8 +1,8 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
-  if ($i % 3 == 0 && $i % 5 == 0) {
+  if ($i % 2 == 0 && $i % 5 == 0) {
   echo "FizzBuzz";
-  } elseif ($i % 3 == 0) {
+  } elseif ($i % 2 == 0) {
     echo "Fizz";
   } elseif ($i % 5 == 0) {
     echo "Buzz";
